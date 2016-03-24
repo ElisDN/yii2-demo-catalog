c0c233373660578c95cfa66ac712d69c4d0e1961
ElisDN <mail@elisdn.ru>
2016-03-24 13:57:37 +0300

    Changed app language

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'language' => 'ru',
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
