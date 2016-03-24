1191bf08e9ad7a8772980056f2597a52fb075654
ElisDN <mail@elisdn.ru>
2016-03-24 17:01:52 +0300

    Added product tags processing

--- models/Product.php
+++ models/Product.php
@@ -127,4 +127,30 @@ class Product extends ActiveRecord
     {
         $this->_tagsArray = (array)$value;
     }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        $this->updateTags();
+        parent::afterSave($insert, $changedAttributes);
+    }
+
+    private function updateTags()
+    {
+        $currentTagIds = $this->getTags()->select('id')->column();
+        $newTagIds = $this->getTagsArray();
+
+        foreach (array_filter(array_diff($newTagIds, $currentTagIds)) as $tagId) {
+            /** @var Tag $tag */
+            if ($tag = Tag::findOne($tagId)) {
+                $this->link('tags', $tag);
+            }
+        }
+
+        foreach (array_filter(array_diff($currentTagIds, $newTagIds)) as $tagId) {
+            /** @var Tag $tag */
+            if ($tag = Tag::findOne($tagId)) {
+                $this->unlink('tags', $tag, true);
+            }
+        }
+    }
 }
