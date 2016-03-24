1f55a433a06cac311aed9cee3f37a12697c0cc4b
ElisDN <mail@elisdn.ru>
2016-03-24 13:31:33 +0300

    Enabled pretty urls

--- config/web.php
+++ config/web.php
@@ -38,14 +38,12 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
